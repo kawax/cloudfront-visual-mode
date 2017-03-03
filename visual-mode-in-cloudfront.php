f48b44716f0ac6066c840190e6b65f61f58e7ba1
kawax <kawaxbiz@gmail.com>
2017-03-03 11:10:59 +0900

    version

--- visual-mode-in-cloudfront.php
+++ visual-mode-in-cloudfront.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:     Enable Visual Mode in CloudFront
+ * Version: 1.0.0
  * Plugin URI:      https://github.com/kawax/visual-mode-in-cloudfront
  * Description:     Enable Visual Mode in CloudFront
  * Author:          kawax
  * Author URI:      https://github.com/kawax
- * Version:         1.0.0
  * License:         GPLv2 or later
  * License URI:     http://www.gnu.org/licenses/gpl-2.0.html
  *
